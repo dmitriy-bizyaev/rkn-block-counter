6597b65d407f7af8d0d882e374e545e88ebe41e5
Dmitriy Bizyaev <dmitriy.bizyaev@gmail.com>
2018-04-17 17:59:12 +0300

    added polling

--- index.js
+++ index.js
@@ -14,40 +14,40 @@ const totalAddresses = 3702258432;
 const templateString = fs.readFileSync('index.ejs', { encoding : 'utf8' } );
 const template = ejs.compile(templateString);
 
-const cacheTime = 60 * 1000; // 1 min
-let lastReq = 0;
-let cached = 0;
+const pollInterval = 5 * 60 * 1000; // 5 min
+let numBlocked = 0;
 
 const getBlockedIpsNum = async () => {
-  const now = Date.now();
-
-  if (now - lastReq <= cacheTime) {
-    return cached;
-  }
-
-  try {
-    const data = await request(url, { json: true });
-    const num = data[data.length - 1].y;
-
-    cached = num;
-    lastReq = now;
-
-    return num;
-  } catch (err) {
-    return cached;
-  }
+  const data = await request(url, { json: true });
+  return data[data.length - 1].y;
 };
 
 const calcPercent = n => n / totalAddresses * 100;
 
+const wait = msecs => new Promise(resolve => setTimeout(resolve, msecs));
+
+const poll = async () => {
+  while (true) {
+    while (true) {
+      try {
+        console.log('Requesting data');
+        numBlocked = await getBlockedIpsNum();
+        console.log('Got data');
+        break;
+      } catch (err) {
+        console.log('Failed to get data');
+      }
+    }
+
+    await wait(pollInterval);
+  }
+};
+
 const app = express();
 
 app.get('/', async (req, res) => {
-  const numBlocked = await getBlockedIpsNum();
-  const percentBlocked = calcPercent(numBlocked);
-
   const html = template({
-    percentBlocked: Math.round(percentBlocked * 10000) / 10000
+    percentBlocked: Math.round(calcPercent(numBlocked) * 10000) / 10000
   });
 
   res.header('Content-Type', 'text/html').end(html);
@@ -59,4 +59,5 @@ const options = argv.option({
   type: 'int',
 }).run().options;
 
+poll();
 app.listen(options.port || 3000);
